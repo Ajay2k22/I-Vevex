b22ff541ff0affdaf7acd6384610a6277d3db9c9
Ajay2k22 <ajaykumar999jaiswal@gmail.com>
2024-04-20 01:00:09 +0530

    Basic template

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,16 @@
 import express from "express";
-import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import createApolloGraphQlServer from "./graphql";
 async function init() {
   const app = express();
   const PORT = Number(process.env.PORT) || 8000;
   app.use(express.json());
   // Create Graphql Server
-  const server = new ApolloServer({
-    typeDefs: `
-        type Query{
-            hello:String
-            sayName(name:String):String
-        }
-    `,
-    resolvers: {
-      Query: {
-        hello: () => "Hello there I am a graphql",
-        sayName: (_, { name }: { name: string }) => `Hey there ${name}`,
-      },
-    },
-  });
-
-  await server.start();
-
   app.get("/", (req, res) => {
     res.json({ message: `server is running at PORT ${PORT}` });
   });
 
-  app.use("/graphql", expressMiddleware(server));
+  app.use("/graphql", expressMiddleware(await createApolloGraphQlServer()));
 
   app.listen(PORT, () => {
     console.log(`Server is listening at ${PORT} http://localhost:${PORT}`);
